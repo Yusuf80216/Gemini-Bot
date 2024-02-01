cba7c50d667aa0d9bc2d378f0fa3763569cd0fd3
Yusuf Ansari <yusuf80216@gmail.com>
2024-02-01 20:55:31 +0530

    v1 deploy

--- APIs/APIs/settings.py
+++ APIs/APIs/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-dhxj$h1on0y78p+ho7a4txp*@@no#j+qmb6^3mk#a^hl_ry1u%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [".vercel.app", '.now.sh']
 
 
 # Application definition
@@ -119,9 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+# STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+import os
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
