b3170892365e7d9968514a6c3c181b03b0644524
Yusuf Ansari <yusuf80216@gmail.com>
2024-02-01 21:03:52 +0530

    ch

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo "BUILD START"
 python3.10 -m pip install -r requirements.txt
-python3.10 manage.py collectstatic --input --clear
+python3.10 manage.py collectstatic --noinput --clear
 echo "BUILD END"
