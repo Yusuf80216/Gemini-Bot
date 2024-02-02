2377f70faa48778c45dd96ecfff2f3ca84e8a113
Yusuf Ansari <yusuf80216@gmail.com>
2024-02-03 00:08:39 +0530

    idk

--- APIs/manage.py
+++ APIs/manage.py
@@ -14,8 +14,9 @@ def main():
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+        )
+    # from exc
+    # execute_from_command_line(sys.argv)
 
 
 if __name__ == "__main__":
